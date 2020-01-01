6171faa69ac2c77ebaddfc8d33bc0bedf87d1ce9
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-01 15:56:50 +0000

    yet even more clean code, exception handling and logging

--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/BaseTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/BaseTest.java
@@ -2,14 +2,17 @@ package ie.murph.testautomationu.webdriver_automation.base;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
+
 public class BaseTest {
-	
+	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private WebDriver driver;
 	private List<WebElement> listOfWebElements;
 	
@@ -18,26 +21,18 @@ public class BaseTest {
 	}
 	
 	public void setUp() throws InterruptedException {
-//		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-//		driver = new ChromeDriverTest().getChromeDriver();
-//		driver.get("https://the-internet.herokuapp.com/");
+		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+		driver = new ChromeDriverTest().getChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
 		
-//		setBrowserSize();
-//		searchingForMulipleElements();
-//		searchingForAnElement();
-//		System.out.println(driver.getTitle());
+		searchingForMulipleElements();
+		searchingForAnElement();
+		searchingForNoSuchElementException();
 		
-		new NavigationTest().setup();
+		System.out.println(driver.getTitle());
 		
-//		Thread.sleep(3000);
-//		driver.quit();
-	}
-	
-	public void setBrowserSize() {
-//		Browser sizes under test
-		driver.manage().window().fullscreen();
-		driver.manage().window().maximize();
-		driver.manage().window().setSize(new Dimension(375, 812)); // Mobile Devices new Dimension(width, height))
+		Thread.sleep(3000);
+		driver.quit();
 	}
 	
 	public void searchingForMulipleElements() {
@@ -48,13 +43,29 @@ public class BaseTest {
 	public void searchingForAnElement() {
 		WebElement tagNameInputs = driver.findElement(By.linkText("Inputs"));
 		tagNameInputs.click();
-//		WebElement noSuchElementException = driver.findElement(By.tagName("Pauly Murphy"));
-//		noSuchElementException.click();
+	}
+	
+	public void searchingForNoSuchElementException() {
+		try
+		{
+			WebElement noSuchElementException = driver.findElement(By.tagName("Pauly Murphy"));
+			noSuchElementException.click();
+		}
+		catch(NoSuchElementException e) 
+		{ 
+			LOGGER.info("Exception toString(): " + e.toString());
+			LOGGER.info("Exception getMessage(): " + e.getMessage());
+			LOGGER.info("Exception  getLocalizedMessage(): " + e.getLocalizedMessage());
+			LOGGER.info("Exception getAdditionalInformation(): " + e.getAdditionalInformation());
+		}
+		
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		BaseTest baseTest = new BaseTest();
 		baseTest.setUp();
+		
+//		new NavigationTest().setup();
 	}
 	
 }
--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/ChromeDriverTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/ChromeDriverTest.java
@@ -1,21 +1,42 @@
 package ie.murph.testautomationu.webdriver_automation.base;
 
+import java.util.logging.Logger;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import ie.murph.testautomationu.webdriver_automation.util.IWebDriverURL;
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 
 public class ChromeDriverTest {
-	
+	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private WebDriver driver;
 	
 	public ChromeDriverTest() {
+		LOGGER.info("++ChromeDriverTest() Constructor");
 		System.setProperty(IWebDriverURL.WEB_DRIVER_PROPERTY_KEY_CHROME, IWebDriverURL.WEB_DRIVER_URL_TO_EXE_VALUE_CHROME);
 		driver = new ChromeDriver();
 	}
 	
 	public WebDriver getChromeDriver() {
+		LOGGER.info("++getChromeDriver()");
 		return driver;
+	}
+	
+	public void setBrowserSizeToFullScreen() {
+		LOGGER.info("++setBrowserSizeToFullScreen()");
+		driver.manage().window().fullscreen();
+	}
+	
+	public void setBrowserSizeToMaximize() {
+		LOGGER.info("++setBrowserSizeToFullScreen()");
+		driver.manage().window().maximize();
+	}
+	
+	public void setBrowserSizeToMaximize(int width, int height) {
+		LOGGER.info("++setBrowserSizeToMaximize( " + width + " " + height + " )");
+		driver.manage().window().setSize(new Dimension(width, height)); // Mobile Devices new Dimension(width, height))
 	}
 	
 }
