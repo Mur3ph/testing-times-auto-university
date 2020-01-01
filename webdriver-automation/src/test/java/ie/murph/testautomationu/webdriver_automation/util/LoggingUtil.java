082aca3c0c5f5f13728a5fa46455961e74a4346f
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-01 14:36:53 +0000

    singleton logging util class to use throughout project

--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/util/LoggingUtil.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/util/LoggingUtil.java
@@ -3,11 +3,15 @@ package ie.murph.testautomationu.webdriver_automation.util;
 import java.util.logging.Logger;
 
 public class LoggingUtil {
-	
-	public static Logger getInstance() {
-	      String callingClassName = 
-	         Thread.currentThread().getStackTrace()[2].getClass().getCanonicalName();
-	      return Logger.getLogger(callingClassName);
-	   }
 
+	// Initialized during class loading
+//	private static final LoggingUtil INSTANCE = new LoggingUtil();
+
+	// To prevent creating another instance of LoggingUtil Singleton
+	private LoggingUtil() {}
+
+	public static Logger getInstance() {
+		String callingClassName = Thread.currentThread().getStackTrace()[2].getClass().getCanonicalName();
+		return Logger.getLogger(callingClassName);
+	}
 }
