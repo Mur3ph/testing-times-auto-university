e2d867a1a24f25762f8dfe73164213f5d8755cd1
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-03 22:33:47 +0000

    added TestNG for assertions, annotations and to run tests

--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/BaseTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/base/BaseTest.java
@@ -1,72 +1,33 @@
 package ie.murph.testautomationu.webdriver_automation.base;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import ie.murph.testautomationu.webdriver_automation.driver.ChromeDriverTest;
+import ie.murph.testautomationu.webdriver_automation.pages.home.Home;
+import ie.murph.testautomationu.webdriver_automation.util.IWebsite;
 import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 
 public class BaseTest {
 	private static final Logger LOGGER = LoggingUtil.getInstance(); 
-	private WebDriver driver;
-	private List<WebElement> listOfWebElements;
+	private ChromeDriverTest chromeDriverTest;
+	protected Home home;
 	
-	public BaseTest() {
-		listOfWebElements = new ArrayList<WebElement>();
+	@BeforeClass
+	public void setUp() {
+		LOGGER.info("++setUp()");
+		chromeDriverTest = new ChromeDriverTest();
+		chromeDriverTest.setWebsiteUnderTest(IWebsite.THE_INTERNET);
+		this.home = new Home(chromeDriverTest.getChromeDriver());
 	}
 	
-	public void setUp() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-		driver = new ChromeDriverTest().getChromeDriver();
-		driver.get("https://the-internet.herokuapp.com/");
-		
-		searchingForMulipleElements();
-		searchingForAnElement();
-		searchingForNoSuchElementException();
-		
-		System.out.println(driver.getTitle());
-		
-		Thread.sleep(3000);
-		driver.quit();
-	}
-	
-	public void searchingForMulipleElements() {
-		listOfWebElements = driver.findElements(By.tagName("a"));
-		System.out.println("Number of Anchor tags: " + listOfWebElements.size());
-	}
-
-	public void searchingForAnElement() {
-		WebElement tagNameInputs = driver.findElement(By.linkText("Inputs"));
-		tagNameInputs.click();
-	}
-	
-	public void searchingForNoSuchElementException() {
-		try
-		{
-			WebElement noSuchElementException = driver.findElement(By.tagName("Pauly Murphy"));
-			noSuchElementException.click();
-		}
-		catch(NoSuchElementException e) 
-		{ 
-			LOGGER.info("Exception toString(): " + e.toString());
-			LOGGER.info("Exception getMessage(): " + e.getMessage());
-			LOGGER.info("Exception  getLocalizedMessage(): " + e.getLocalizedMessage());
-			LOGGER.info("Exception getAdditionalInformation(): " + e.getAdditionalInformation());
-		}
-		
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		BaseTest baseTest = new BaseTest();
-		baseTest.setUp();
-		
-//		new NavigationTest().setup();
+	@AfterClass
+	public void teardown() {
+		LOGGER.info("++teardown()");
+		chromeDriverTest.delayBrowserClosingBy3Seconds();
+		chromeDriverTest.shutBrowser();
 	}
 	
 }
--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/login/LoginTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/login/LoginTest.java
@@ -1,44 +1,43 @@
 package ie.murph.testautomationu.webdriver_automation.login;
 
-import ie.murph.testautomationu.webdriver_automation.driver.ChromeDriverTest;
-import ie.murph.testautomationu.webdriver_automation.pages.home.Home;
+import ie.murph.testautomationu.webdriver_automation.base.BaseTest;
+
 import ie.murph.testautomationu.webdriver_automation.pages.login.Login;
 import ie.murph.testautomationu.webdriver_automation.pages.login.SecureArea;
-import ie.murph.testautomationu.webdriver_automation.util.IWebsite;
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 import ie.murph.testautomationu.webdriver_automation.util.Secure;
 
-public class LoginTest {
-	private ChromeDriverTest chromeDriverTest;
-	private Home home;
+import static org.testng.Assert.*;
+
+import java.util.logging.Logger;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest{
+	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private Login login;
 	private SecureArea secureArea;
 	
-	public static void main(String[] arguments) {
-		LoginTest baseFrameworkTest = new LoginTest();
-		baseFrameworkTest.setUp();
-		baseFrameworkTest.formAuthenticationFunctionalityTest();
-		baseFrameworkTest.loginFunctionalityTest();
-		baseFrameworkTest.successfulLoginTest();
+	@BeforeMethod
+	public void formAuthenticationFunctionalityTest() {
+		LOGGER.info("++formAuthenticationFunctionalityTest()");
+		this.login = home.clickFormAuthenticationLink();
 	}
 	
-	private void setUp() {
-		chromeDriverTest = new ChromeDriverTest();
-		chromeDriverTest.setWebsiteUnderTest(IWebsite.THE_INTERNET);
-		this.home = new Home(chromeDriverTest.getChromeDriver());
-	}
-	
-	private void formAuthenticationFunctionalityTest() {
-		this.login = this.home.clickFormAuthenticationLink();
-	}
-	
-	private void loginFunctionalityTest() {
+	@BeforeMethod
+	public void loginFunctionalityTest() {
+		LOGGER.info("++loginFunctionalityTest()");
 		this.login.setUsernameTextBox(Secure.THE_INTERNET_LOGIN_PAGE_USERNAME.toString());
 		this.login.setPasswordTextBox(Secure.THE_INTERNET_LOGIN_PAGE_PASSWORD.toString());
 		this.secureArea = this.login.clickLoginButton();
 	}
 	
-	private void successfulLoginTest() {
-		this.secureArea.getSuccessfulWelcomeText();
+	@Test(priority=3)
+	public void successfulLoginTest() {
+		LOGGER.info("++successfulLoginTest()");
+		String expectedResult = "You logged into a secure area!"; 
+		assertTrue(this.secureArea.getSuccessfulWelcomeText().contains(expectedResult));
 	}
 
 }
