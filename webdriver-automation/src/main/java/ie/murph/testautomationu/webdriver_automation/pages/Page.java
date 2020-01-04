f68e1b8cf44d376a7bbc42eae8ebe45b19fbf713
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-04 15:53:19 +0000

    added page object for dropdown and interface for cleaner design

---
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/Page.java
@@ -0,0 +1,7 @@
+package ie.murph.testautomationu.webdriver_automation.pages;
+
+public interface Page {
+	
+	String clickUrl(String url);
+
+}
---
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/dropdown/Dropdown.java
@@ -0,0 +1,19 @@
+package ie.murph.testautomationu.webdriver_automation.pages.dropdown;
+
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
+
+public class Dropdown {
+	private static final Logger LOGGER = LoggingUtil.getInstance();
+	private WebDriver driver;
+	private By successfulLoginWelcomeBanner = By.id("dropdown");
+
+	public Dropdown(WebDriver driver) {
+		LOGGER.info("++Dropdown(" + driver + " )");
+		this.driver = driver;
+	}
+}
