5d5425e08af5e0dccc06090a9db96a1a485bd9a6
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-05 15:29:05 +0000

    page object model for dropdwn complete,now to test it

--- webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/dropdown/Dropdown.java
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/dropdown/Dropdown.java
@@ -1,9 +1,13 @@
 package ie.murph.testautomationu.webdriver_automation.pages.dropdown;
 
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import ie.murph.testautomationu.webdriver_automation.pages.Page;
 import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
@@ -11,12 +15,23 @@ import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 public class Dropdown extends Page{
 	private static final Logger LOGGER = LoggingUtil.getInstance();
 	private WebDriver driver;
-	private By successfulLoginWelcomeBanner = By.id("dropdown");
+	private By dropdown = By.id("dropdown");
 
 	public Dropdown(WebDriver driver) {
 		LOGGER.info("++Dropdown(" + driver + " )");
 		this.driver = driver;
 	}
 	
+	public void selectFromDropdownList(String option) {
+		findDropdownElement().selectByVisibleText(option);
+	}
 	
+	public List<String> getAllSelectedOptionsFromDropdown() {
+		List<WebElement> listOfSelectedElements = findDropdownElement().getAllSelectedOptions();
+		return listOfSelectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
+	}
+	
+	private Select findDropdownElement() {
+		return new Select(driver.findElement(dropdown));
+	}
 }
