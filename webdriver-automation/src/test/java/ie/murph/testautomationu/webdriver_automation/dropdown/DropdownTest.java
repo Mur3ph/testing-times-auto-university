ed3f38b9f0a5d788383049ac2c137771f30ba83c
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-09 22:33:59 +0000

    dropdown page object test, BUT, strange happenings when I use two test methods with before method annotations

--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/dropdown/DropdownTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/dropdown/DropdownTest.java
@@ -1,10 +1,13 @@
 package ie.murph.testautomationu.webdriver_automation.dropdown;
 
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 import ie.murph.testautomationu.webdriver_automation.base.BaseTest;
 import ie.murph.testautomationu.webdriver_automation.pages.dropdown.Dropdown;
@@ -13,6 +16,8 @@ import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 public class DropdownTest extends BaseTest{
 	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private Dropdown dropdown;
+	private List<String> selectedOptionsFromDropdown;
+	private static final String OPTION_1 = "Option 1";
 	
 	@BeforeMethod
 	public void gotoDropdownPage() {
@@ -20,14 +25,29 @@ public class DropdownTest extends BaseTest{
 		this.dropdown = home.clickDropdownLink();
 	}
 	
-	@BeforeMethod
-	public void dropdownFunctionality() {
-		LOGGER.info("++dropdownFunctionality()");
+	@BeforeMethod(dependsOnMethods= { "gotoDropdownPage" })
+	public void selectOptionFromDropdownList() {
+		LOGGER.info("++selectOptionFromDropdownList()");
+		this.dropdown.selectFromDropdownList(OPTION_1);
+	}
+	
+	@BeforeMethod(dependsOnMethods= { "selectOptionFromDropdownList" })
+	public void getSelectedOptionFromDropdownList() {
+		LOGGER.info("++getSelectedOptionFromDropdownList()");
+		this.selectedOptionsFromDropdown = this.dropdown.getAllSelectedOptionsFromDropdown();
 	}
 	
 	@Test(priority=1)
-	public void Test() {
-		LOGGER.info("++Test()");
+	public void isSelectedOptionTest() {
+		LOGGER.info("++isSelectedOptionTest()");
+		assertEquals(this.selectedOptionsFromDropdown.size(), 1, "Incorrect number of selected options from dropdown list");
+		assertTrue(this.selectedOptionsFromDropdown.contains(OPTION_1), OPTION_1 + " not selected");
 	}
+	
+//	@Test(priority=2)
+//	public void selectedOptionTest() {
+//		LOGGER.info("++selectedOptionTest()");
+//		assertTrue(this.selectedOptionsFromDropdown.contains(OPTION_1), OPTION_1 + " not selected");
+//	}
 
 }
