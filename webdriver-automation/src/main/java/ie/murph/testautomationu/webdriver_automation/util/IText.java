a8c5213815d3f5e406e8514f22e5c58bacc1e651
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-13 22:34:07 +0000

    global contant interface

--- webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/util/IText.java
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/util/IText.java
@@ -6,4 +6,5 @@ public interface IText
 	public final static String NAVIGATION_MENU_BUTTONS = "Number of Anchor tags, only on Menu buttons: ";
 	public final static String CONSTRUCTOR = "Constructor: Declaring Chrome Web Driver and setting website URL";
 	public final static String DROPDOWN_PAGE_OPTION_1 = "Option 1";
+	public final static String SECURE_PAGE_LOGIN_SUCCESS_EXPECTED_MESSAGE = "You logged into a secure area!"; 
 }
