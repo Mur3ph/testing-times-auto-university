cac00cce6379671f415a30fb225e58c654864214
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-19 20:55:32 +0000

    key press pom functionality, fill input area and retrice label text result

--- webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/keypress/KeyPress.java
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/keypress/KeyPress.java
@@ -1,13 +1,24 @@
 package ie.murph.testautomationu.webdriver_automation.pages.keypress;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class KeyPress {
 	
 	private WebDriver driver;
+	private By inputField = By.id("target");
+	private By labelResultFromInputField = By.id("result");
 	
 	public KeyPress(WebDriver driver) {
 		this.driver = driver;
+	}
+	
+	public void enterText(String text) {
+		driver.findElement(inputField).sendKeys(text);
+	}
+	
+	public String getLabel() {
+		return driver.findElement(labelResultFromInputField).getText();
 	}
 
 }
