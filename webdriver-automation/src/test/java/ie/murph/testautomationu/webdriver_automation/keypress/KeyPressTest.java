23896dc22386652fcca9de3e2dea06fb84b130ee
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-19 21:12:11 +0000

    back space test, passed

---
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/keypress/KeyPressTest.java
@@ -0,0 +1,40 @@
+package ie.murph.testautomationu.webdriver_automation.keypress;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ie.murph.testautomationu.webdriver_automation.base.BaseTest;
+import ie.murph.testautomationu.webdriver_automation.pages.keypress.KeyPress;
+
+import static org.testng.Assert.*;
+
+public class KeyPressTest extends BaseTest{
+	
+	private KeyPress keyPress;
+	
+	@BeforeClass
+	public void gotoKeyPressPage() {
+//		LOGGER.info("++gotoKeyPressPage()");
+		this.keyPress = home.clickKeyPressesLink();
+	}
+	
+	@BeforeMethod
+	public void sendTextToKeyPressInputBox() {
+//		LOGGER.info("++sendTextToKeyPressInputBox()");
+	}
+	
+	@Test(priority=1)
+	public void backSpaceTest() {
+		this.keyPress.enterText("A" + Keys.BACK_SPACE);
+		assertEquals(this.keyPress.getLabel(), "You entered: BACK_SPACE");
+	}
+	
+	@Test(priority=2)
+	public void pieTest() {
+		this.keyPress.enterText(Keys.ALT + "227");
+		assertEquals(this.keyPress.getLabel(), "You entered: BACK_SPACE");
+	}
+
+}
