66eeabaf8673fe5794b33722fc3c9f5e929a58f5
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-19 21:29:47 +0000

    logging for key press pom and key press tests

--- webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/keypress/KeyPress.java
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/keypress/KeyPress.java
@@ -1,32 +1,42 @@
 package ie.murph.testautomationu.webdriver_automation.pages.keypress;
 
+import java.util.logging.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
+
 public class KeyPress {
-	
+	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private WebDriver driver;
 	private By inputField = By.id("target");
 	private By labelResultFromInputField = By.id("result");
 	
 	public KeyPress(WebDriver driver) {
+		LOGGER.info("++KeyPress( " + driver + " )");
 		this.driver = driver;
 	}
 	
 	public void enterPi() {
+		LOGGER.info("++enterPi()");
 		enterText(Keys.chord(getPiAsciiCodeWindows()) + "=3.14");
+		LOGGER.info("--enterPi( " + Keys.chord(getPiAsciiCodeWindows()) + "=3.14 )");
 	}
 	
 	public void enterText(String text) {
+		LOGGER.info("++enterText( " + text + " )");
 		driver.findElement(inputField).sendKeys(text);
 	}
 	
 	public String getPiAsciiCodeWindows() {
+		LOGGER.info("++getPiAsciiCodeWindows()");
 		return Keys.ALT + "227";
 	}
 	
 	public String getLabel() {
+		LOGGER.info("++getLabel()");
 		return driver.findElement(labelResultFromInputField).getText();
 	}
 
--- webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/keypress/KeyPressTest.java
+++ webdriver-automation/src/test/java/ie/murph/testautomationu/webdriver_automation/keypress/KeyPressTest.java
@@ -7,32 +7,37 @@ import org.testng.annotations.Test;
 
 import ie.murph.testautomationu.webdriver_automation.base.BaseTest;
 import ie.murph.testautomationu.webdriver_automation.pages.keypress.KeyPress;
+import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
 
 import static org.testng.Assert.*;
 
+import java.util.logging.Logger;
+
 public class KeyPressTest extends BaseTest{
-	
+	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private KeyPress keyPress;
 	
 	@BeforeClass
 	public void gotoKeyPressPage() {
-//		LOGGER.info("++gotoKeyPressPage()");
+		LOGGER.info("++gotoKeyPressPage()");
 		this.keyPress = home.clickKeyPressesLink();
 	}
 	
 	@BeforeMethod
 	public void sendTextToKeyPressInputBox() {
-//		LOGGER.info("++sendTextToKeyPressInputBox()");
+		LOGGER.info("++sendTextToKeyPressInputBox()");
 	}
 	
 	@Test(priority=1)
 	public void backSpaceTest() {
+		LOGGER.info("++backSpaceTest()");
 		this.keyPress.enterText("A" + Keys.BACK_SPACE);
 		assertEquals(this.keyPress.getLabel(), "You entered: BACK_SPACE");
 	}
 	
 	@Test(priority=2)
 	public void pieTest() {
+		LOGGER.info("++pieTest()");
 		this.keyPress.enterPi();
 //		assertEquals(this.keyPress.getLabel(), "You entered: BACK_SPACE");
 	}
