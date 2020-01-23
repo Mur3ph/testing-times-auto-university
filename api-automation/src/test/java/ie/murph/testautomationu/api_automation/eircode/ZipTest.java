01bc2cf81ba4f554a26ac509b505ddc32574b0d5
Mur3ph <paulmurphymainstreet@gmail.com>
2020-01-23 17:21:54 +0000

    failing api test scenario and rest assurred syntax setup, execution and verification

--- api-automation/src/test/java/ie/murph/testautomationu/api_automation/eircode/ZipTest.java
+++ api-automation/src/test/java/ie/murph/testautomationu/api_automation/eircode/ZipTest.java
@@ -10,12 +10,22 @@ public class ZipTest {
 	
 	@Test
 	public void ZipRequestTest() {
+		given(). 	// Setup
+        when().		// Execution
+            get("http://zippopotam.us/us/90210").
+        then().		// Verification
+            assertThat().
+            body("places[0].'place name'", equalTo("Beverly Hills"));
+	}
+	
+	@Test
+	public void ZipRequestFailTest() {
 		given().
         when().
             get("http://zippopotam.us/us/90210").
         then().
             assertThat().
-            body("places[0].'place name'", equalTo("Beverly Hills"));
+            body("places[0].'place name'", not(equalTo("Beijing")));
 	}
 
 }
