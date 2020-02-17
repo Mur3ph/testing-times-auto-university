461cd7f675b8ba84063075e33a6dfa99c42b9385
Mur3ph <paulmurphymainstreet@gmail.com>
2020-02-17 22:56:59 +0000

    finding js button via xpath and clicking on that button method

--- webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/alert/JavaScriptAlert.java
+++ webdriver-automation/src/main/java/ie/murph/testautomationu/webdriver_automation/pages/alert/JavaScriptAlert.java
@@ -2,20 +2,27 @@ package ie.murph.testautomationu.webdriver_automation.pages.alert;
 
 import java.util.logging.Logger;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import ie.murph.testautomationu.webdriver_automation.util.IText;
 import ie.murph.testautomationu.webdriver_automation.util.LoggingUtil;
+import ie.murph.testautomationu.webdriver_automation.util.XPath;
 
 public class JavaScriptAlert {
 	private static final Logger LOGGER = LoggingUtil.getInstance(); 
 	private WebDriver driver;
+	private By javascriptTriggeredAlertButton = By.xpath(XPath.JAVASRIPT_ALERTS_PAGE_JAVASCRIPT_BUTTON.toString());
 
 	public JavaScriptAlert(WebDriver driver) {
 		LOGGER.info("++JavaScriptAlert( " + driver + " ) " + IText.CONSTRUCTOR);
 		this.driver = driver;
 	}
 	
+	public void triggerJavaScriptAlertButton() {
+		LOGGER.info("++triggerJavaScriptAlertButton()");
+		driver.findElement(javascriptTriggeredAlertButton).click();
+	}
 	
 	
 }
